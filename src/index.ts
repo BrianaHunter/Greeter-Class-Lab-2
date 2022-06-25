8f76b75b564fc8b5cf2eadc58426fec54acf7232
Briana Hunter <brianahunter@Brianas-MacBook-Pro.local>
2022-06-25 13:55:01 -0400

    console.log testing

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ let greeter: Greeter = new Greeter("Hello, Everyone!");
 console.log(greeter.greeting);
 
 import { JavaScriptGreeter } from "../src/JavaScriptGreeter";
-let javaGreeter: JavaScriptGreeter = new JavaScriptGreeter("Hello Grant");
+let javaGreeter: JavaScriptGreeter = new JavaScriptGreeter("Hello, Grant!");
 console.log(javaGreeter.greeting);
 
 import { LoudGreeter } from "../src/LoudGreeter";
+let loudGreeter: LoudGreeter = new LoudGreeter("Hello, Amelia!!");
+console.log(loudGreeter.greeting);
 
-import {} from "../src/";
+import { HtmlGreeter } from "../src/HtmlGreeter";
+let htmlGreeter: HtmlGreeter = new HtmlGreeter("<h1>Hello, Paige</h1>");
+console.log(htmlGreeter.greeting);
